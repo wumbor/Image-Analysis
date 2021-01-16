a56b212783e072688fcd80f4cd38b14b07665de0
wumbor <kumbolvictor@gmail.com>
2021-01-16 18:25:55 +0100

    Improve presentation of graph
    -Automatically resize image to avoid cluttering
    -Show Experiment# in graph
    -Arrange treatments in a more reasonable order

--- R Scripts/SummariseNucleiCount.R
+++ R Scripts/SummariseNucleiCount.R
@@ -22,7 +22,7 @@ analysis_log_file <- paste(experiment_id, "_AnalysisLog.txt", sep = "")
 
 #Specify output files
 results_excel_file <- paste(experiment_id, "_Nuclei_Count.xlsx", sep = "")
-results_graph_file <- paste(experiment_id, "_Graph.tiff", sep = "")
+results_graph_file <- paste(experiment_id, "_Nuclei_Count.tiff", sep = "")
 
 #Specify header line in microscopy_sequence_file
 img_sequence_header_line <- (grep("Condition", read_lines(microscopy_sequence_file, skip = 0, skip_empty_rows = FALSE, n_max = -1L, na = character()))) - 1
@@ -67,35 +67,34 @@ null_group_stats <- combined_data %>%
 #Normalize counts
 combined_data <- combined_data %>%
   mutate(Treatment = trimws(Treatment)) %>%
-  mutate(Mean_Normalized_Count = normalize(Count, null_group_stats$avg)) %>%
-  arrange(desc(Treatment))
+  mutate(Mean_Normalized_Count = normalize(Count, null_group_stats$avg))
 
+#Rearrange data to preferred presentation order
+TreatmentLevels <- c("Null", "miR#4", "miR#5", "miR#5(L)", "miR#5(M)", "miR#5(H)", "miR#13", "miR#13(L)", "miR#13(M)", "miR#13(H)", "miR#19", "miR#19(L)", "miR#19(M)", "miR#19(H)", "miR#27", "miR#27(L)", "miR#27(M)", "miR#27(H)", "let7b", "LOX", "R848")
+combined_data$Treatment <- factor(combined_data$Treatment, levels = TreatmentLevels)
+combined_data <- combined_data %>%
+  arrange(Treatment)
 
 #Calculate group means and medians
 combined_data_summary <- combined_data %>%
   group_by(Treatment) %>%
   summarise(Normalized_Mean = mean(Mean_Normalized_Count))
 
-#Arrange summarised data in preferred format
-TreatmentLevels <- c("Null", "miR#4", "miR#5", "miR#5(L)", "miR#5(M)", "miR#5(H)", "miR#13", "miR#13(L)", "miR#13(M)", "miR#13(H)", "miR#19", "miR#19(L)", "miR#19(M)", "miR#19(H)", "miR#27", "miR#27(L)", "miR#27(M)", "miR#27(H)", "let7b", "LOX", "R848")
-combined_data_summary$Treatment <- factor(combined_data_summary$Treatment, levels = TreatmentLevels)
-combined_data_summary <- combined_data_summary %>%
-  # select(Treatment, Normalized_Mean) %>% #select only mean normalized values
-  arrange(Treatment)
 
 #write data to file
 if (file.access(results_excel_file)){
-  write.xlsx(combined_data, file = results_excel_file, sheetName = "AutoThreshold Nuclei Count", col.names = TRUE, row.names = FALSE, append = TRUE)
+  write.xlsx(combined_data, file = results_excel_file, sheetName = "Nuclei Count", col.names = TRUE, row.names = FALSE, append = TRUE)
 } else {
-  write.xlsx(combined_data, file = results_excel_file, sheetName = "AutoThreshold Nuclei Count", col.names = TRUE, row.names = FALSE, append = FALSE)
+  write.xlsx(combined_data, file = results_excel_file, sheetName = "Nuclei Count", col.names = TRUE, row.names = FALSE, append = FALSE)
 }
 write.xlsx(combined_data_summary, file = results_excel_file, sheetName = "Nuclei Count Summary", col.names = TRUE, append = TRUE)
 write.xlsx(analysis_parameters, file = results_excel_file, sheetName = "Analysis Parameters", col.names = FALSE, row.names = FALSE, append = TRUE)
 
 
 #save graph of results
-tiff(results_graph_file, width=900, height=800, res=100)
-ggbarplot(combined_data, x = "Treatment", y = "Mean_Normalized_Count", add = c("mean_se", "jitter"), size = 1)
+scaled_width = 100*length(unique(combined_data_summary$Treatment))
+tiff(results_graph_file, width=scaled_width, height=800, res=100)
+ggbarplot(combined_data, x = "Treatment", y = "Mean_Normalized_Count", add = c("mean_se", "jitter"), size = 1, ylab = "Normalised NeuN+ Cell Count (%)", title = experiment_id, lab.hjust = 0.5) + theme(plot.title = element_text(hjust = 0.5))
 # median_plot <- ggbarplot(combined_data, x = "Treatment", y = "Median_Normalized_Count", add = c("mean_se", "jitter"), color = "Treatment", palette = "npg", size = 1)
 # ggarrange(mean_plot, median_plot, labels = c("Normalised by Mean", "Normalised by Median"), ncol = 2, common.legend = TRUE, legend = "bottom")
 garbage <- dev.off()
