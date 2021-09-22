3401397499bd4b31e250cca91ee746cbd933ef3d
wumbor <kumbolvictor@gmail.com>
2021-09-22 13:45:59 +0200

    Update AnalyseMiRNAKinetics.R
    - Updates the metaresults table automatically
    - Standard variable and function names

--- R Scripts/AnalyseMiRNAKinetics.R
+++ R Scripts/AnalyseMiRNAKinetics.R
@@ -1,12 +1,15 @@
-# This script analyses data from the miRDistribution macro to plot graphs of kinetics of miRNA uptake
-#Written by Victor Kumbol, August 2021
+# This script analyses the data output of the miRKinetics macro to generate miRNA uptake kinetics graphs
+# Written by Victor Kumbol, August 2021
 
 #load required libraries
 library(tidyverse)
 library("xlsx")
 library(ggpubr)
 library(ggsci)
-library(bayestestR)
+
+
+source("C:/Users/Victor Kumbol/Documents/GitHub/Image-Analysis/R Scripts/GenericFunctions.R", chdir = TRUE)
+
 
 #define custom functions
 extractImageParameter <- function(parameterName, imageParameters) { 
@@ -18,7 +21,7 @@ extractImageParameter <- function(parameterName, imageParameters) {
 }
 
 
-processUptakeData <- function(imageDataFolder) {
+processKineticsData <- function(imageDataFolder) {
   
   originalWorkingDirectory <- getwd()
   setwd(imageDataFolder)
@@ -64,14 +67,16 @@ processUptakeData <- function(imageDataFolder) {
 
 
 
-exportResults <- function(combinedData, timeOffset, frameInterval, resultsExcelFile) { #This function exports data tables
+exportResults <- function(combinedData, timeOffset, frameInterval, sliceSelection, resultsExcelFile) { #This function exports data tables
   
   if (file.exists(resultsExcelFile)){   #overwrite any existing results file
     file.remove(resultsExcelFile)
   } 
   
   combinedDataPerSlice <- combinedData %>% 
+    filter(Slice %in% sliceSelection) %>%
     select(-Area) %>%
+    filter(!is.na(Mean)) %>% #slight edit here to handle NAs
     pivot_wider(names_from = Parameter, values_from = Mean) %>% 
     mutate(prop = red/(red + redInverse)) %>% #compute other parameters
     pivot_longer(red:prop, names_to = "Parameter", values_to = "Value") %>%
@@ -79,7 +84,7 @@ exportResults <- function(combinedData, timeOffset, frameInterval, resultsExcelF
     mutate(AbsoluteTime.h = RecordingTime.h + (timeOffset/60)) %>%
     group_by(Filename, Channel, Parameter, Slice) %>%
     mutate(NormalisedValue = (Value/first(Value))) %>%
-    ungroup()
+    ungroup() 
   combinedDataPerSlice <- left_join(combinedDataPerSlice, ParameterLabels, by = "Parameter")
   combinedDataPerSlice <- left_join(combinedDataPerSlice, ChannelLabels, by = "Channel")
   
@@ -89,25 +94,39 @@ exportResults <- function(combinedData, timeOffset, frameInterval, resultsExcelF
     group_by(Filename, AbsoluteTime.h, ChannelLabel, Region) %>%
     summarise(Mean = mean(Value),  Mean.SD = sd(Value), NormalisedMean = mean(NormalisedValue), NormalisedMean.SD = sd(NormalisedValue))
   
-  # write.xlsx(as.data.frame(combinedDataPerFrame), file = resultsExcelFile, sheetName = "combinedDataPerFrame", col.names = TRUE, row.names = FALSE, append = TRUE)
+  write.xlsx(as.data.frame(combinedDataPerFrame), file = resultsExcelFile, sheetName = "combinedDataPerFrame", col.names = TRUE, row.names = FALSE, append = TRUE)
   
   kineticsPlots <- list()
-  #Plot graph of proportion
+  combinedDataPerSlice <- combinedDataPerSlice %>%
+    filter(AbsoluteTime.h <= 4) #plot only 4h results
+  
+  #Plot graph of proportion with SD
   p1 <- combinedDataPerSlice %>%
     filter(Region %in% c("Endosomal Proportion")) %>%
     filter(ChannelLabel%in% c("miRNA", "DAPI", "phRodored")) %>%
     ggline(x= "AbsoluteTime.h", y = "Value", color ="ChannelLabel", palette =  c("#0000FF", "#00FF00", "#FF0000"), numeric.x.axis = TRUE, xticks.by = 1, ylab = "Endosomal/Non-Endosomal Proportion", add = "mean_sd")
   kineticsPlots <- append(kineticsPlots, list(p1))
   
-  #Plot graph of Mean Fluorescence with SE
+  #Plot graph of Mean Fluorescence with SD
   p2 <- combinedDataPerSlice %>%
     filter(Region=="Endosomal") %>%
     ggline(x= "AbsoluteTime.h", y = "Value", color ="ChannelLabel", palette =  c("#0000FF", "#00FF00", "#FF0000"), numeric.x.axis = TRUE, xticks.by = 1, ylab = "Mean Fluorescence Intensity", add = "mean_sd")
   kineticsPlots <- append(kineticsPlots, list(p2))
   
-  kineticsPlots <- ggarrange(plotlist=kineticsPlots, nrow = 2)
+  #Plot graph of Normalised Mean Fluorescence with SE
+  p3 <- combinedDataPerSlice %>%
+    filter(Region=="Endosomal") %>%
+    ggline(x= "AbsoluteTime.h", y = "NormalisedValue", color ="ChannelLabel", palette =  c("#0000FF", "#00FF00", "#FF0000"), numeric.x.axis = TRUE, xticks.by = 1, ylab = "Normalised Fluorescence Intensity", add = "mean_sd")
+  kineticsPlots <- append(kineticsPlots, list(p3))
+  
+  kineticsPlots <- ggarrange(plotlist=kineticsPlots, nrow = 3)
   return(kineticsPlots)
 }
+
+
+
+
+
 
 
 
@@ -132,15 +151,6 @@ sliceSelectionRange <- str_split(sliceSelectionRange, "-", simplify = TRUE)
 sliceSelection <- seq(sliceSelectionRange[1], sliceSelectionRange[2],by=1)
 
 
-frameInterval <- 30 #override frame inteval for trial experiments
-
-
-
-
-
-
-
-
 
 #define designations for parameters
 ChannelLabels <- tibble(Channel = c("red", "green", "blue"), ChannelLabel = c("phRodored", "miRNA", "DAPI"))
@@ -152,14 +162,15 @@ resultsGraphFile <- paste(treatment, "_Kinetics.tiff", sep = "")
 
 
 
-#request user to select a source folder 
-DataToExport  <- processUptakeData(imageDataFolder)
-
-DataToExport <- DataToExport %>% #filter for selected slices
-  filter(Slice %in% sliceSelection)
-
-Graphs <- exportResults(DataToExport, timeOffset, frameInterval, resultsExcelFile)
-
-tiff(resultsGraphFile, width = 1500, height = 1500, res=100)
+#run data analysis
+DataToExport  <- processKineticsData(imageDataFolder)
+Graphs <- exportResults(DataToExport, timeOffset, frameInterval, sliceSelection, resultsExcelFile)
+tiff(resultsGraphFile, width = 1500, height = 2250, res=100)
 annotate_figure(Graphs, top = text_grob(treatment, face = "bold", size = 14))
 garbage <- dev.off()
+
+#update meta results sheet
+ParameterAnalyzed <- "MiRKinetics"
+metaResultsFile <- metaResultFilemMORPH
+updateMetaResults(experimentId, ParameterAnalyzed, resultsExcelFile, metaResultsFile)
+
